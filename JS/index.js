db01a0307f63ab141248b7943b2fa34a07e5618e
AkshayS734 <manasshukla876@gmail.com>
2023-07-27 18:13:39 +0530

    Made buttons

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,6 @@
-var arr=Array["rock","paper","scissor"];
-var random=Math.random()*arr.length;
-random=Math.floor(random);
-var computerchoice=arr[random];
-var numberofEvents =document.querySelectorAll(".button").length;
+var numberofEvents =document.querySelectorAll(".buttons").length;
 for (var i=0;i<numberofEvents;i++){
-    document.querySelectorAll(".button")[i].addEventListener("click",function (){
+    document.querySelectorAll(".buttons")[i].addEventListener("click",function (){
 
         var buttonInnerHTML=this.innerHTML;
         giveWinner(buttonInnerHTML);
@@ -13,9 +9,14 @@ for (var i=0;i<numberofEvents;i++){
         
 });
 }
-var playercount=0;
-var computercount=0;
+
 function giveWinner(event){
+    var playercount=0;
+    var computercount=0;
+    var arr=Array["rock","paper","scissor"];
+    var random=Math.random()*arr.length;
+    random=Math.floor(random);
+    var computerchoice=arr[random];
     if((event=="rock" && computerchoice=="scissor")||(event=="paper" && computerchoice=="rock")||(event=="scissor" && computerchoice=="paper")){
         playercount++;
     }
@@ -25,7 +26,13 @@ function giveWinner(event){
     else{
         computercount++;
     }
+    var score1=document.querySelector(".player");
+    score1.textContent=playercount;
+    var score2=document.querySelector(".computer");
+    score2.textContent=computercount;
+
 }
+
 
 
 
