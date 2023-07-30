4ea472eb30a9a10b1ec272230b0d53822824e48e
AkshayS734 <manasshukla876@gmail.com>
2023-07-30 10:45:35 +0530

    Added javascript, now it is working

--- JS/index.js
+++ JS/index.js
@@ -1,51 +1,81 @@
-var numberofEvents =document.querySelectorAll(".buttons").length;
-for (var i=0;i<numberofEvents;i++){
-    document.querySelectorAll(".buttons")[i].addEventListener("click",function (){
+var numberOfEvents = document.querySelectorAll(".button").length;
 
-        var buttonInnerHTML=this.innerHTML;
-        giveWinner(buttonInnerHTML);
-        buttonAnimation(buttonInnerHTML);
+// Loop through each button and add event listeners
+for (var i = 0; i < numberOfEvents; i++) {
+  document.querySelectorAll(".button")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    giveWinner(buttonInnerHTML);
+    button.classList.add("clicked");
+    setTimeout(function () {
+        button.classList.remove("clicked");
+    }, 200);
 
-        
-});
+
+  });
 }
+var playerCount = 0;
+var computerCount = 0;
+var maxPoints = 5;
 
-function giveWinner(event){
-    var playercount=0;
-    var computercount=0;
-    var arr=Array["rock","paper","scissor"];
-    var random=Math.random()*arr.length;
-    random=Math.floor(random);
-    var computerchoice=arr[random];
-    if((event=="rock" && computerchoice=="scissor")||(event=="paper" && computerchoice=="rock")||(event=="scissor" && computerchoice=="paper")){
-        playercount++;
-    }
-    else if(event===computerchoice){
+function giveWinner(event) {
+  var arr = ["rock", "paper", "scissor"];
+  var random = Math.floor(Math.random() * arr.length);
+  var computerChoice = arr[random];
 
-    }
-    else{
-        computercount++;
-    }
-    var score1=document.querySelector(".player");
-    score1.textContent=playercount;
-    var score2=document.querySelector(".computer");
-    score2.textContent=computercount;
+  if ((event === "rock" && computerChoice === "scissor") ||
+      (event === "paper" && computerChoice === "rock") ||
+      (event === "scissor" && computerChoice === "paper")) {
+    playerCount++;
+    handleGameResult("Player wins!");
+  } else if (event === computerChoice) {
+    handleGameResult("It's a tie!");
+  } else {
+    computerCount++;
+    handleGameResult("Computer wins!");
+  }
+  updateScores();
+
+  var score1 = document.querySelector(".player");
+  score1.textContent = playerCount;
+  var score2 = document.querySelector(".computer");
+  score2.textContent = computerCount;
 
 }
-
-
-
-
-
-
-
-
-
-
-function buttonAnimation(currentKey){
-    var activeButton=document.querySelector("."+currentKey);
-    activeButton.classList.add("pressed");
-    setTimeout(function(){
-        activeButton.classList.remove("pressed");
-    },100);
+function handleGameResult(result) {
+    var roundResult = document.querySelector(".result");
+    roundResult.textContent = "Round Result: " + result;
+  
+    if (playerCount >= maxPoints || computerCount >= maxPoints) {
+      // If any player reaches the maximum points, stop the game and show the final result
+      document.querySelectorAll(".button").forEach(button => {
+        button.disabled = true; // Disable the buttons
+      });
+  
+        var finalResult = document.querySelector(".final-result");
+        if (playerCount > computerCount) {
+            finalResult.textContent = "Final Winner: Player";
+        } 
+        else if (playerCount < computerCount) {
+            finalResult.textContent = "Final Winner: Computer";
+        } 
+        else {
+            finalResult.textContent = "Final Result: It's a Tie!";
+        }
+    }
+}
+function updateScores() {
+    var score1 = document.querySelector(".player");
+    score1.textContent = playerCount;
+    var score2 = document.querySelector(".computer");
+    score2.textContent = computerCount;
+}
+function restartGame() {
+    playerCount = 0;
+    computerCount = 0;
+    updateScores(); // Update the displayed scores
+    document.querySelector(".result").textContent = "Round Result: ";
+    document.querySelector(".final-result").textContent = "Final Winner: ";
+    document.querySelectorAll(".button").forEach(button => {
+    button.disabled = false; // Re-enable the buttons
+    });
 }
